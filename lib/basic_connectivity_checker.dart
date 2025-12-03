f4783d274e889c1a3b28ed0bf9a84c816cf25afe
Lorena <lorena@Lorenas-MacBook-Pro.local>
2025-12-03 21:01:57 +0200

    small fix

--- lib/basic_connectivity_checker.dart
+++ lib/basic_connectivity_checker.dart
@@ -1,4 +1,3 @@
-/// Expose your connectivity checker and related types
 export 'src/connectivity_checker.dart';
 export 'src/connectivity_checker_constants.dart';
 export 'src/connectivity_status.dart';
