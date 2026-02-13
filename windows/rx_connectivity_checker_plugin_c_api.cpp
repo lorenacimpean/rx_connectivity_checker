4756ab9d0e1d7011cb29e0a02ef98540fc622630
Lorena <lorena@Lorenas-MacBook-Pro-2.local>
2026-02-13 20:27:29 +0200

    updated header file name

--- windows/rx_connectivity_checker_plugin_c_api.cpp
+++ windows/rx_connectivity_checker_plugin_c_api.cpp
@@ -1,4 +1,4 @@
-#include "rx_connectivity_checker/rx_connectivity_checker_plugin_c_api.h"
+#include "rx_connectivity_checker/windows_rx_connectivity_checker.h"
 
 #include <flutter/plugin_registrar_windows.h>
 
